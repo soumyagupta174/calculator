a7ecadf0513d7a7d7c5b2034f86ae6ee014b16be
Soumya Gupta <soumyagupta174@gmail.com>
2024-05-28 23:39:50 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,23 @@
+let display = document.getElementById('display');
+let expression = '';
+
+function addToDisplay(value) {
+  expression += value;
+  display.value = expression;
+}
+
+function clearDisplay() {
+  expression = '';
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    let result = eval(expression);
+    display.value = result;
+    expression = '';
+  } catch (error) {
+    display.value = 'Error';
+    expression = '';
+  }
+}
